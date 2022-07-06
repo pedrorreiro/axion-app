c546c7f4200675209af525979baef5aba392db8e
Pedro <pedroriqueal17@gmail.com>
2022-07-06 14:45:28 -0300

    pagina de login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import bg from './img/bg.jpg';
+import logo from './img/logo.png';
+import mail from './img/mail.png';
+import lock from './img/lock.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='loginBox'>
+        <img src={logo} alt="logo"></img>
+
+        <p>Email</p>
+        <div className='input'>
+          <input type='text' placeholder='seunome@email.com'></input>
+          <img className="iconInput" src={mail} alt="icone"></img>
+        </div>
+
+        <p>Password</p>
+        <div className='input'>
+          <input type='password' placeholder='Password'></input>
+          <img className="iconInput" src={lock} alt="iconez"></img>
+        </div>
+        
+        <div id='check'>
+          <input type='checkbox'></input>
+          <label>Mostrar a senha.</label>
+        </div>
+
+        <p id='problemas'>Problemas para acessar sua conta?</p>
+
+        <div id='button'>
+          Acessar
+        </div>
+
+        <div id='or'>
+          <div className="linha"></div>
+          ou
+          <div className="linha"></div>
+        </div>
+
+        <div id='cadastrar'>
+          Cadastrar
+        </div>
+
+        <p>Termos de uso · Política de privacidade</p>
+        
+
+      </div>
+      <img src={bg} id="fundo" alt='background'></img>
+
     </div>
   );
 }
